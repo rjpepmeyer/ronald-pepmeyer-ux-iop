9de85bf1e3a70f2c8a6d1755bf5f3870cad885a1
Ronald Pepmeyer <ronaldpepmeyer@Ronalds-MacBook-Air.local>
2015-09-17 14:30:35 -0500

    Added 'gulp help' support and dynamic generation of .css from .scss/.sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
-var gulp = require('gulp-help')(require('gulp'));
-var gulpconnect = require('gulp-connect');
+'use strict';
+
+var gulp    = require('gulp-help')(require('gulp'));
+var connect = require('gulp-connect');
+var sass    = require('gulp-sass');
 
 gulp.task('default', 'Hosts /src and watches for changes', ['connect','watch']);
 
 gulp.task('connect', 'Hosts /src at localhost:1820', function () {
-  gulpconnect.server({
+  connect.server({
     port: 1820,
     livereload: true,
     fallback: 'user_profile.html'
@@ -12,9 +15,16 @@ gulp.task('connect', 'Hosts /src at localhost:1820', function () {
 });
 
 gulp.task('reload', 'Reloads files from /src to host', function() {
-  gulpconnect.reload();
+  connect.reload();
+});
+
+gulp.task('sass', 'Returns .css from .scss and .sass files', function() {
+  gulp.src(['./styles/*.scss', './styles/*.sass'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./styles'));
 });
 
 gulp.task('watch', 'Watches for changes in /src', function() {
   gulp.watch('src/*', ['reload']);
+  gulp.watch(['./styles/*.scss', './styles/*.sass'], ['sass']);
 });
