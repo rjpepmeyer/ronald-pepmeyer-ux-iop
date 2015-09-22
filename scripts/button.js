7167e1eb74f18ef7f94944e15b8ec5b940fc39b1
Ronald Pepmeyer <ronaldpepmeyer@Ronalds-MacBook-Air.local>
2015-09-22 10:20:42 -0500

    Seperate scripts from html

---
+++ scripts/button.js
@@ -0,0 +1,9 @@
+function toggle(x,y) {
+  if (document.getElementById(x).style.display == 'inline') {
+    document.getElementById(x).style.display = 'none';
+    document.getElementById(y).innerHTML = 'Show more info';
+  } else {
+    document.getElementById(x).style.display = 'inline';
+    document.getElementById(y).innerHTML = 'Show less info';
+  }
+}
