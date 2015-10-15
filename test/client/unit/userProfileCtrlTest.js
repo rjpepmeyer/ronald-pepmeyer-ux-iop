9b0fee08d2df1977014a87fece1d01cca24cd434
Ronald Pepmeyer <ronaldpepmeyer@Ronalds-MacBook-Air.local>
2015-10-15 15:32:33 -0500

    (7.1) Working edit test, nonworking delete test

--- test/client/unit/userProfileCtrlTest.js
+++ test/client/unit/userProfileCtrlTest.js
@@ -5,6 +5,7 @@ describe('userProfileController', function() {
 	var url = 'http://localhost:24149/users';
 	var testUser = {firstName: 'Bruce', lastName: 'Wayne', email:
 	'bwayne@example.com', phone: '(111) 222-3333', _id: 1};
+	var testUsers = [testUser];
 
 	// USER LIST TEST
 
@@ -16,10 +17,29 @@ describe('userProfileController', function() {
 		httpBackend.expectGET(url).respond(200, [testUser]);
 	}));
 
-	it('queries the user list from the server', function () {
+	it('queries the user list from the server', function() {
 		httpBackend.expectGET(url).respond(200, [testUser]);
     httpBackend.flush();
 		expect(scope.user.firstName).toEqual(testUser.firstName);
 	});
+
+	it('edits the users information', function() {
+		httpBackend.expectGET(url).respond(200, [testUser]);
+		httpBackend.expectPUT(url + '/1').respond(200);
+		scope.userCopy = {};
+		scope.userCopy.phone = '(333) 222-1111';
+		scope.editUser();
+		httpBackend.flush();
+		expect(scope.user.phone).toEqual(scope.userCopy.phone);
+	});
+
+	it('deletes the user', function() {
+		httpBackend.expectGET(url).respond(200, testUsers);
+		httpBackend.expectDELETE(url + '/1').respond(200);
+		scope.user = testUser;
+		scope.deleteUser(scope.user);
+		httpBackend.flush();
+		expect(scope.user).toEqual(undefined);
+	})
 
 });
