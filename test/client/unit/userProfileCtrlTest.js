39cef084e820d4db90b7a74b98a889ce5e049309
Ronald Pepmeyer <ronaldpepmeyer@Ronalds-MacBook-Air.local>
2015-10-15 13:53:01 -0500

    (7.1) Unit test for basic functionality of user profile controller

--- test/client/unit/userProfileCtrlTest.js
+++ test/client/unit/userProfileCtrlTest.js
@@ -1,24 +1,25 @@
-describe('usersListController', function() {
+describe('userProfileController', function() {
 	beforeEach(module('onboarding'));
 
-	var scope, controller;
+	var scope, controller, stateParams;
 	var url = 'http://localhost:24149/users';
 	var testUser = {firstName: 'Bruce', lastName: 'Wayne', email:
-	'bwayne@example.com', phone: '(111) 222-3333'};
+	'bwayne@example.com', phone: '(111) 222-3333', _id: 1};
 
 	// USER LIST TEST
 
 	beforeEach(inject(function($injector, $rootScope, $controller) {
 		scope = $rootScope.$new();
 		httpBackend = $injector.get ('$httpBackend');
-		controller = $controller('usersListController', {$scope: scope});
+		controller = $controller('userProfileController', {$scope: scope,
+			$stateParams: {id: 1}});
 		httpBackend.expectGET(url).respond(200, [testUser]);
 	}));
 
 	it('queries the user list from the server', function () {
 		httpBackend.expectGET(url).respond(200, [testUser]);
     httpBackend.flush();
-		expect(scope.users[0].firstName).toEqual(testUser.firstName);
+		expect(scope.user.firstName).toEqual(testUser.firstName);
 	});
 
 });
