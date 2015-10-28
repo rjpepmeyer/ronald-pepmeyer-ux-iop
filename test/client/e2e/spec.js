665ee8ff282eb36249cf7a6412ff7b0169e9a64b
Ronald Pepmeyer <ronaldpepmeyer@Ronalds-MacBook-Air.local>
2015-10-28 16:48:15 -0500

    (7.2) closer to working

--- test/client/e2e/spec.js
+++ test/client/e2e/spec.js
@@ -1,28 +1,27 @@
 'use strict';
 
-browser.get('http://localhost:1820/'); /*
+browser.get('http://localhost:1820/');
 onboardingDev = angular.module('myAppDev', ['onboarding', 'ngMockE2E']);
 onboardingDev.run(function($httpBackend) { */
 
-	describe('onboarding project', function() {
+describe('onboarding project', function() {
 
-		var $httpBackend = ngMockE2E.$httpBackend;
-		var url = 'http://localhost:1820/#/usersList/';
-		var endpoint = 'http://localhost:24149/users';
+	var ngMockE2E = require('ng-mock-e2e');
+	var $httpBackend = ngMockE2E.$httpBackend;
+	var url = 'http://localhost:1820/#/usersList/';
+	var endpoint = 'http://localhost:24149/users';
 
-		beforeEach(function () {
-		  ngMockE2E.addMockModule();
-		  ngMockE2E.addAsDependencyForModule('onboarding');
-		  ngMockE2E.embedScript('../node_modules/angular-mocks/angular-mocks.js');
-		});
-
-		it('load the users list page', function() {
-			$httpBackend.whenGET(endpoint).respond('[{firstName: Bruce}]');
-			browser.get(url);
-			expect(browser.getCurrentUrl()).toBe(browser.baseUrl + url);
-			expect(browser.getTitle()).toEqual('Onboarding (Ronald Pepmeyer)');
-		});
-
+	beforeEach(function () {
+	  ngMockE2E.addMockModule();
+	  ngMockE2E.addAsDependencyForModule('onboarding');
+	  ngMockE2E.embedScript('../node_modules/angular-mocks/angular-mocks.js');
 	});
 
-//});
+	it('load the users list page', function() {
+		$httpBackend.whenGET(endpoint).respond('[{firstName: Bruce}]');
+		browser.get(url);
+		expect(browser.getCurrentUrl()).toBe(browser.baseUrl + url);
+		expect(browser.getTitle()).toEqual('Onboarding (Ronald Pepmeyer)');
+	});
+
+});
