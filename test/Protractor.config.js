e43554ea3b2579c9dd28eaaff4f5eed8e67a4d57
Ronald Pepmeyer <ronaldpepmeyer@Ronalds-MacBook-Air.local>
2015-11-04 16:21:22 -0600

    (7.2) experimental

--- test/Protractor.config.js
+++ test/Protractor.config.js
@@ -1,5 +1,8 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-	capabilities: { 'browserName': 'chrome' },
-  specs: ['client/e2e/mocks.js', 'client/e2e/spec.js']
+  seleniumAddress: 'http://localhost:4444/wd/hub',/*
+	capabilities: { 'browserName': 'chrome' },*/
+  specs: ['client/e2e/spec.js'],
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 10000
+  }
 };
