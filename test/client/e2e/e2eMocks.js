38642916dc94d648e6debe5fe9ebbf4ea97bb10c
Ronald Pepmeyer <ronaldpepmeyer@Ronalds-MacBook-Air.local>
2015-11-19 13:04:05 -0600

    (7.2.2) Updated E2E tests to display and test changes to data

--- test/client/e2e/e2eMocks.js
+++ test/client/e2e/e2eMocks.js
@@ -4,26 +4,36 @@ exports.mockFunction = function() {
 
   var user1 = {firstName: 'Bruce', lastName: 'Wayne', phone: '(111) 222-3333',
   email: 'batman@example.com', _id: '1'};
-  var user1Edit = user1;
+  var user1Edit = angular.copy(user1);
   user1Edit.phone = '(123) 456-7890';
+
   var user2 = {firstName: 'Clark', lastName: 'Kent', phone: '(222) 333-4444',
   email: 'superman@example.com', _id: '2'};
+
   var user3 = {firstName: 'Harvey', lastName: 'Dent', phone: '(333) 444-5555',
   email: 'twoface@example.com'};
+  var user3withID = angular.copy(user3);
+  user3withID._id = '3';
+
+  var userArray = [user1, user2];
+
 	var endpoint = 'http://localhost:24149/users';
 
   angular.module('userMock', ['ngMockE2E'])
   .run(function ($httpBackend) {
 		$httpBackend.whenGET(endpoint).respond(function(){
-      return [200, [user1, user2]];
+      return [200, userArray];
     });
     $httpBackend.whenPOST(endpoint, user3).respond(function(){
-      return [200, ''];
+      userArray.push(user3withID);
+      return [200, user3withID];
     });
     $httpBackend.whenPUT(endpoint.concat('/1'), user1Edit).respond(function(){
+      userArray[0] = user1Edit;
       return [200];
     });
     $httpBackend.whenDELETE(endpoint.concat('/1')).respond(function(){
+      userArray = userArray.slice(1);
       return [200];
     });
     $httpBackend.whenGET(/.*/).passThrough();
--- test/client/e2e/e2eSpec.js
+++ test/client/e2e/e2eSpec.js
@@ -11,58 +11,94 @@ describe('onboarding project', function() {
 		browser.addMockModule('userMock', mocks.mockFunction);
 	});
 
-	it('loads the users list page', function() {
-		browser.get(url.concat('usersList/'));
-		expect(browser.getCurrentUrl()).toBe(url + 'usersList/');
-		expect(element(by.id('uiview')).getText()).toContain('Bruce Wayne');
+	afterEach(function() {
+		browser.sleep(750);
 	});
 
-	it('loads the \'user profile\' screen', function() {
+	it('loads the \'users list\' page by default', function() {
+		//Load url without route and get redirected to usersList
+		browser.get(url);
+		expect(browser.getCurrentUrl()).toBe(url + 'usersList/');
+	});
+
+	it('displays the users', function () {
+		//UserList displays users
+		expect(element(by.id('uiview')).getText()).toContain('Bruce Wayne');
+		expect(element(by.id('uiview')).getText()).toContain('Clark Kent');
+	});
+
+	it('navigates to the \'user profile\' screen', function() {
+		//Click on a user and load their profile
 		element(by.linkText('Bruce Wayne')).click();
 		expect(browser.getCurrentUrl()).toBe(url + 'userProfile/1');
 		expect(element(by.id('uiview')).getText()).toContain('Show less info');
-	});
-
-	it('loads the correct user', function() {
 		expect(element(by.id('uiview')).getText()).toContain('batman@example.com');
 	});
 
-	it('loads \'edit user\' screen', function() {
+	it('navigates to the \'edit user\' screen', function() {
+		//Click on 'edit user' from profile and load edit user screen
 		element(by.linkText('Edit User')).click();
 		expect(element(by.id('uiview')).getText()).toContain('Phone number');
 	});
 
 	it('edits the user', function() {
+		var oldPhone = '(111) 222-3333';
+		var newPhone = '(123) 456-7890';
+
+		//Pretest old phone value
+		expect(element(by.model('userCopy.phone')).getAttribute('value'))
+		.toContain(oldPhone);
+
+		//Filling out form
 		element(by.model('userCopy.phone')).clear();
-		element(by.model('userCopy.phone')).sendKeys('(123) 456-7890');
+		element(by.model('userCopy.phone')).sendKeys(newPhone);
 		element(by.id('editButton')).click();
+
+		//Return to user profile with new info
 		expect(element(by.id('uiview')).getText()).toContain('Show less info');
-		// "Show less info" means it went to the profile, which indicates success
+		expect(element(by.id('uiview')).getText()).toContain(newPhone);
 	});
 
-	it('loads \'delete user\' screen', function() {
+	it('navigates to the \'delete user\' screen', function() {
+		//Click on 'Delete User' and load 'Delete User' screen
 		element(by.linkText('Delete User')).click();
 		expect(element(by.id('uiview')).getText()).toContain('forever');
 	});
 
 	it('deletes the user', function() {
+		//Click on 'Delete' button and return to usersList without that user
 		element(by.id('deleteButton')).click();
 		expect(element(by.id('uiview')).getText()).toContain('Clark Kent');
-		//"Clark Kent" means it went back to the user list, which indicates success
+		expect(element(by.id('uiview')).getText()).not.toContain('Bruce Wayne');
 	});
+
+	it('navigates to the \'create user\' page', function () {
+		//Click on 'Create New User' and load createNewUser
+		element(by.linkText('Create New User')).click();
+		expect(browser.getCurrentUrl()).toBe(url + 'createNewUser/');
+	})
 
 	it('creates a new user', function() {
 		var user3 = {firstName: 'Harvey', lastName: 'Dent', phone: '(333) 444-5555',
 	  email: 'twoface@example.com'};
-		element(by.linkText('Create New User')).click();
-		expect(browser.getCurrentUrl()).toBe(url + 'createNewUser/');
+
+		// Filling out form
 		element(by.model('user.firstName')).sendKeys(user3.firstName);
 		element(by.model('user.lastName')).sendKeys(user3.lastName);
 		element(by.model('user.phone')).sendKeys(user3.phone);
 		element(by.model('user.email')).sendKeys(user3.email);
 		element(by.id('submitButton')).click();
+
+		// Returns to that user's profile
 		expect(element(by.id('uiview')).getText()).toContain('Show less info');
-		// "Show less info" means it went to the profile, which indicates success
+		expect(element(by.id('uiview')).getText()).toContain('Harvey');
+	});
+
+	it('reflects creation and deletion events in usersList', function() {
+		element(by.linkText('Select User')).click();
+		expect(element(by.id('uiview')).getText()).not.toContain('Bruce Wayne');
+		expect(element(by.id('uiview')).getText()).toContain('Clark Kent');
+		expect(element(by.id('uiview')).getText()).toContain('Harvey Dent');
 	});
 
 });
